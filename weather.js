56bdee32bd2d6d667f3b8c29b428ba9fc27ae22a
Mihail_Olaru <mihai.olaru007@gmail.com>
2022-05-07 17:49:59 +0300

    Integrated actions selector based on the args.

--- weather.js
+++ weather.js
@@ -5,6 +5,16 @@ const initCLI = () => {
 	//Getting the cms args. You can optionally use tools like Yargs for this purpose.
 	const args = getArgs(process.argv);	
 	console.log(args);
+	if (args.h){
+		//if arg is 'h' output the help info.
+	}
+	if (args.s){
+		//If arg is 's', save the city.
+	}
+	if (args.t){
+		//If arg is 't', save the token.
+	}
+	//if none of the above display the weather or nay other action.
 };
 
 initCLI();
